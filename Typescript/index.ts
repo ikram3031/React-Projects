36aece2f68ab4f9c1cc7c9e06a9f370219281a9a
ikram3031 <ikramul.web@gmail.com>
2025-12-14 01:59:31 +0600

    9:30

---
+++ Typescript/index.ts
@@ -0,0 +1,13 @@
+class Person {
+    public name: string;
+    public age: number;
+
+
+    constructor (name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let p1 = new Person ("Ikramul", 29)
+ 
